68becbf03dd9fad34d994de164ed6ef4176a64ab
JL <129757125+TanJiaLong@users.noreply.github.com>
2023-07-01 14:40:07 +0800

    Login as Guest

--- htdocs/barterlt/php/load_item.php
+++ htdocs/barterlt/php/load_item.php
@@ -1,54 +1,97 @@
 <?php
-	if(!isset($_POST)){
-		$response = array("staus"=>"failed","data"=>null);
-		sendJsonResponse($response);
-		die();
-	}
+if(!isset($_POST)){
+	$response = array("staus"=>"failed","data"=>null);
+	sendJsonResponse($response);
+	die();
+}
+include_once('dbconnect.php');
 
-	include_once('dbconnect.php');
+//set each page to display 6 records
+$results_per_page = 6;
 
+//set current page number to be displayed
+if(isset($_POST['pageNo'])){
+	$pageNo = $_POST['pageNo'];
+}else{
+	$pageNo = 1;
+}
+
+$first_result_for_that_page = ($pageNo - 1) * $results_per_page;
+
+//check categories first
+if(isset($_POST['selectedCategory']) && !empty($_POST['selectedCategory'])){
+	$selectedCategory = "'".$_POST['selectedCategory']."'";
+
+	//item page display the 'seller'
 	if(isset($_POST['userid'])){
-		$userid = $_POST["userid"];
-		$sqlloaditems = "SELECT * FROM `tbl_items` WHERE user_id = '$userid'";
+		if($_POST['userid'] != 'na'){
+			$userid = $_POST["userid"];
+			$sqlloaditems = "SELECT * FROM `tbl_items` WHERE `item_category` IN ($selectedCategory) AND user_id = '$userid'";
+		}else{
+			$sqlloaditems = "SELECT * FROM `tbl_items` WHERE `item_category` IN ($selectedCategory)";
+		}
 	}
-	if(isset($_POST['search'])){
+	//search page (with search keywords)
+	else if(isset($_POST['search'])){
 		$search = $_POST["search"];
-		$sqlloaditems = "SELECT * FROM `tbl_items` WHERE item_name LIKE '%$search%'";
+		$sqlloaditems = "SELECT * FROM `tbl_items` WHERE `item_category` IN ($selectedCategory) AND (item_name LIKE '%$search%' OR item_desc LIKE '%$search%')";
+	}
+	//search page
+	else{
+		$sqlloaditems = "SELECT * FROM `tbl_items` WHERE `item_category` IN ($selectedCategory)";
+	}
+}else{
+	if(isset($_POST['userid'])){
+		if($_POST['userid'] != 'na'){
+			$userid = $_POST["userid"];
+			$sqlloaditems = "SELECT * FROM `tbl_items` WHERE user_id = '$userid'";
+		}else{
+			$sqlloaditems = "SELECT * FROM `tbl_items`";
+		}
+	}else if(isset($_POST['search'])){
+		$search = $_POST["search"];
+		$sqlloaditems = "SELECT * FROM `tbl_items` WHERE item_name LIKE '%$search%' OR item_desc LIKE '%$search%'";
 	}else{
 		$sqlloaditems = "SELECT * FROM `tbl_items`";
 	}
-	
-	$result = $conn->query($sqlloaditems);
+}
 
-	if($result->num_rows > 0){
-		$items["items"] = array();
+//Pagination
+$result = $conn->query($sqlloaditems);
+$numberOfResult = $result->num_rows;
+$numberOfPage = ceil($numberOfResult / $results_per_page);
+$sqlloaditems = $sqlloaditems . "LIMIT $first_result_for_that_page, $results_per_page";
+$result = $conn->query($sqlloaditems);
 
-		while($row = $result->fetch_assoc()){
-			$itemlist = array();
+if($result->num_rows > 0){
+	$items["items"] = array();
+	while($row = $result->fetch_assoc()){
+		$itemlist = array();
 			
-			$itemlist['item_id'] = $row['item_id'];
-			$itemlist['user_id'] = $row['user_id'];
-			$itemlist['item_name'] = $row['item_name'];
-			$itemlist['item_desc'] = $row['item_desc'];
-			$itemlist['item_category'] = $row['item_category'];
-			$itemlist['item_value'] = $row['item_value'];
-			$itemlist['state'] = $row['state'];
-			$itemlist['locality'] = $row['locality'];
-			$itemlist['latitude'] = $row['latitude'];
-			$itemlist['longitude'] = $row['longitude'];
-			$itemlist['reg_date'] = $row['reg_date'];
+		$itemlist['item_id'] = $row['item_id'];
+		$itemlist['user_id'] = $row['user_id'];
+		$itemlist['item_name'] = $row['item_name'];
+		$itemlist['item_desc'] = $row['item_desc'];
+		$itemlist['item_category'] = $row['item_category'];
+		$itemlist['item_quantity'] = $row['item_quantity'];
+		$itemlist['item_value'] = $row['item_value'];
+		$itemlist['state'] = $row['state'];
+		$itemlist['locality'] = $row['locality'];
+		$itemlist['latitude'] = $row['latitude'];
+		$itemlist['longitude'] = $row['longitude'];
+		$itemlist['reg_date'] = $row['reg_date'];
 			
-			array_push($items['items'], $itemlist);
-		}
-		$response = array('status'=> 'success', 'data'=> $items);
-		sendJsonResponse($response);
-	} else {
-		$response = array('status'=> 'failed', 'data'=> null);
-		sendJsonResponse($response);
+		array_push($items['items'], $itemlist);
 	}
+	$response = array('status'=> 'success', 'data'=> $items, 'numberOfPage'=> "$numberOfPage", "numberOfResult"=>"$numberOfResult");
+	sendJsonResponse($response);
+} else {
+	$response = array('status'=> 'failed', 'data'=> null, 'sql'=> $sqlloaditems);
+	sendJsonResponse($response);
+}
 
-	function sendJsonResponse($sentArray){
-		header("Content-Type: application/json");
-		echo json_encode($sentArray);
-	}
+function sendJsonResponse($sentArray){
+	header("Content-Type: application/json");
+	echo json_encode($sentArray);
+}
 ?>
